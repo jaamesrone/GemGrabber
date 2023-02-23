6ca63c413183b092b1f7049c1d86246271aacce1
jamesrone <32492411+jaamesrone@users.noreply.github.com>
2023-02-23 10:16:07 -0800

    Updated Player Controller
    added camera rotation, better rotation, and jumping

--- GemGrabber/Assets/Scripts/PlayerController.cs
+++ GemGrabber/Assets/Scripts/PlayerController.cs
@@ -1,14 +1,18 @@
 using UnityEngine;
-using System.Collections.Generic;
-using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 10f; // adjust as needed
     public float sideStepDistance = 2f; // adjust as needed
+    public float rotationAngle = 90f; // adjust as needed
+    public float rotationSpeed = 10f; // adjust as needed
     public float cameraFollowSpeed = 5f; // adjust as needed
+    public float jumpHeight = 2f; // adjust as needed
+    public float jumpTime = 0.5f; // adjust as needed
     public Transform cameraTransform; // assign in inspector
     private Vector3 cameraOffset;
+    private bool isJumping = false;
+    private float jumpTimer = 0f;
 
     void Start()
     {
@@ -20,18 +24,58 @@ public class PlayerController : MonoBehaviour
         // move forward at a constant speed
         transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
 
-        // move left or right based on keyboard input
+        // rotate left or right based on keyboard input
         if (Input.GetKeyDown(KeyCode.A))
         {
-            transform.Translate(Vector3.left * sideStepDistance);
+            RotateCharacter(-1);
         }
         else if (Input.GetKeyDown(KeyCode.D))
         {
-            transform.Translate(Vector3.right * sideStepDistance);
+            RotateCharacter(1);
         }
 
-        // adjust camera position to follow player horizontally
+        // move left or right based on current rotation
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-transform.right * sideStepDistance * Time.deltaTime, Space.World);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(transform.right * sideStepDistance * Time.deltaTime, Space.World);
+        }
+
+        // adjust camera position to follow player
         Vector3 targetCameraPosition = transform.position + cameraOffset;
         cameraTransform.position = Vector3.Lerp(cameraTransform.position, targetCameraPosition, cameraFollowSpeed * Time.deltaTime);
+        cameraTransform.LookAt(transform.position, Vector3.up);
+
+        // jump
+        if (!isJumping && Input.GetKeyDown(KeyCode.Space))
+        {
+            isJumping = true;
+            jumpTimer = 0f;
+        }
+
+        if (isJumping)
+        {
+            float normalizedTime = jumpTimer / jumpTime;
+
+            // apply jump force using a parabolic curve
+            float jumpForce = jumpHeight * 4f * normalizedTime * (1f - normalizedTime);
+            transform.Translate(Vector3.up * jumpForce * Time.deltaTime, Space.World);
+
+            jumpTimer += Time.deltaTime;
+
+            if (jumpTimer >= jumpTime)
+            {
+                isJumping = false;
+            }
+        }
+    }
+
+    void RotateCharacter(int direction)
+    {
+        transform.Rotate(Vector3.up, direction * rotationAngle);
+        cameraOffset = Quaternion.Euler(0, direction * rotationAngle, 0) * cameraOffset;
     }
 }
